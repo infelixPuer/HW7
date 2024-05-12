d2e5464702cea1d926960850773a37fcb06b498a
Aliaksandr Kulaha <comp.science.learning.2509@gmail.com>
2024-05-12 21:30:23 +0200

    Make Task 2

--- main.js
+++ main.js
@@ -10,9 +10,7 @@ function promiseAll(promises) {
                     results.push(result);
                     completedPromises++;
 
-                    if (completedPromises === promises.length) {
-                        resolve(results);
-                    }
+                    if (completedPromises === promises.length) resolve(results);
                 })
                 .catch(error => {
                     reject(error);
@@ -21,16 +19,50 @@ function promiseAll(promises) {
     });
 }
 
-const promises = [
+const promisesAll = [
     Promise.resolve(1),
     Promise.resolve(2),
     Promise.resolve(3)
 ];
 
-promiseAll(promises)
+promiseAll(promisesAll)
     .then(results => {
         console.log("All promises resolved:", results); // Expected: [1, 2, 3]
     })
     .catch(error => {
         console.error("At least one promise rejected:", error);
+    });
+
+// Task 2: Implement promiseAllSettled Function
+function promiseAllSettled(promises) {
+    return new Promise((resolve, reject) => {
+        let results = [];
+        let settledPromises = 0;
+
+        promises.forEach(promise => {
+            Promise.resolve(promise)
+                .then(value=> results.push({ status: "fulfilled", value: value }),
+                    reason => results.push({ status: "rejected", reason: reason })
+                )
+                .finally(() => {
+                    settledPromises++;
+
+                    if (settledPromises === promises.length) resolve(results);
+                });
+        });
+    });
+}
+
+const promisesAllSettled = [
+    Promise.resolve(1),
+    Promise.reject("Error occurred"),
+    Promise.resolve(3)
+];
+
+promiseAllSettled(promisesAllSettled)
+    .then(results => {
+        console.log("All promises settled:", results);
+        // Expected: [{ status: 'fulfilled', value: 1 },
+        //            { status: 'rejected', reason: 'Error occurred' },
+        //            { status: 'fulfilled', value: 3 }]
     });
