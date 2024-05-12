a1359b9ba10cd41868155abd21d76185f0c72260
Aliaksandr Kulaha <comp.science.learning.2509@gmail.com>
2024-05-12 22:19:31 +0200

    Make Task 4

--- main.js
+++ main.js
@@ -110,3 +110,35 @@ chainPromises(functionsArray)
     .catch(error => {
         console.error("Chained promise error:", error);
     });
+
+// Task 4: Implement promisify Function
+function promisify(callback) {
+    return (arg) => {
+        return new Promise((resolve, reject) => {
+            callback(arg, (error, value) => {
+                if (error) reject(error);
+                else resolve(value);
+            });
+        });
+    };
+}
+
+function callbackStyleFunction(value, callback) {
+    setTimeout(() => {
+        if (value > 0) {
+            callback(null, value * 2);
+        } else {
+            callback("Invalid value", null);
+        }
+    }, 1000);
+}
+
+const promisedFunction = promisify(callbackStyleFunction);
+
+promisedFunction(3)
+    .then(result => {
+        console.log("Promised function result:", result); // Expected: 6
+    })
+    .catch(error => {
+        console.error("Promised function error:", error);
+    });
